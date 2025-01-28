8ff90db1c13e330da94aa0aa11b61135b15f71e9
Roman Zarvirov <zarvirovroman@gmail.com>
2025-01-28 17:10:04 +0000

    blur behaviour fix rollback to square one

--- client/src/components/Chat/Input/ChatForm.tsx
+++ client/src/components/Chat/Input/ChatForm.tsx
@@ -42,7 +42,6 @@ const ChatForm = ({ index = 0 }) => {
 
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isScrollable, setIsScrollable] = useState(false);
-  const [shouldAutoFocus, setShouldAutoFocus] = useState(true);
 
   const SpeechToText = useRecoilValue(store.speechToText);
   const TextToSpeech = useRecoilValue(store.textToSpeech);
@@ -130,10 +129,10 @@ const ChatForm = ({ index = 0 }) => {
   });
 
   useEffect(() => {
-    if (!isSearching && textAreaRef.current && !disableInputs && shouldAutoFocus) {
+    if (!isSearching && textAreaRef.current && !disableInputs) {
       textAreaRef.current.focus();
     }
-  }, [isSearching, disableInputs, shouldAutoFocus]);
+  }, [isSearching, disableInputs]);
 
   useEffect(() => {
     if (textAreaRef.current) {
@@ -157,11 +156,7 @@ const ChatForm = ({ index = 0 }) => {
 
   return (
     <form
-      onSubmit={methods.handleSubmit((data) => {
-        setShouldAutoFocus(false);
-        submitMessage(data);
-        textAreaRef.current?.blur();
-      })}
+      onSubmit={methods.handleSubmit((data) => submitMessage(data))}
       className={cn(
         'mx-auto flex flex-row gap-3 px-4 transition-all duration-200 last:mb-2',
         'mb-4',
@@ -211,10 +206,6 @@ const ChatForm = ({ index = 0 }) => {
                     onPaste={handlePaste}
                     onKeyDown={handleKeyDown}
                     onKeyUp={handleKeyUp}
-                    onFocus={() => {
-                      setShouldAutoFocus(true);
-                      if (isCollapsed) setIsCollapsed(false);
-                    }}
                     onHeightChange={() => {
                       if (textAreaRef.current) {
                         const scrollable = checkIfScrollable(textAreaRef.current);
@@ -227,6 +218,8 @@ const ChatForm = ({ index = 0 }) => {
                     tabIndex={0}
                     data-testid="text-input"
                     rows={1}
+                    onFocus={() => isCollapsed && setIsCollapsed(false)}
+                    onClick={() => isCollapsed && setIsCollapsed(false)}
                     style={{ height: 44, overflowY: 'auto' }}
                     className={cn(
                       baseClasses,
